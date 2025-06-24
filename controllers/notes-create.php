40c442d0846e26b1bc4064234095688df8e79e08
SHIVA NAIDU <dasarisambasivanaidu7@gmail.com>
2025-06-24 17:44:35 +0530

    Add: validation class for note body input and display error messages

--- controllers/notes-create.php
+++ controllers/notes-create.php
@@ -1,18 +1,29 @@
 <?php
 
-$config = require('config.php');
+require 'Validator.php';
+
+$config = require 'config.php';
 $db = new Database($config['database']);
 
 $heading = 'Create Notes Page';
 
 if ($_SERVER["REQUEST_METHOD"] == "POST"){
-    $db->query(
-    'INSERT INTO notess (body, user_id) VALUES (:body, :user_id)',
-    [
-        'body' => $_POST['body'],
-        'user_id' => 1, // Hardcoded current user ID for now
-    ]
-);
+
+    $errors = [];
+
+
+    if (! Validator::string($_POST['body'],1,1000)){
+        $errors['body'] = 'Body is should not more than 1000 characters.';
+    }
+    
+    if (empty($errors)){
+            $db->query('INSERT INTO notess (body, user_id) VALUES (:body, :user_id)',
+                [
+                    'body' => $_POST['body'],
+                    'user_id' => 1, // Hardcoded current user ID for now
+                ]
+            );
+    }
 }
 
 
--- views/notes-create.view.php
+++ views/notes-create.view.php
@@ -10,9 +10,14 @@
         <div class="col-span-full">
           <label for="body" class="block text-sm/6 font-medium text-gray-900">Body</label>
           <div class="mt-2">
-            <textarea name="body" id="body" rows="3" class="block w-full rounded-md bg-white px-3 py-1.5 text-base text-gray-900 outline-1 -outline-offset-1 outline-gray-300 placeholder:text-gray-400 focus:outline-2 focus:-outline-offset-2 focus:outline-indigo-600 sm:text-sm/6"></textarea>
+            <textarea name="body" id="body" rows="3" class="block w-full rounded-md bg-white px-3 py-1.5 text-base text-gray-900 outline-1 -outline-offset-1 outline-gray-300 placeholder:text-gray-400 focus:outline-2 focus:-outline-offset-2 focus:outline-indigo-600 sm:text-sm/6"><?= htmlspecialchars($_POST['body'] ?? '') ?></textarea>
           </div>
-          <p class="mt-3 text-sm/6 text-gray-600">Write a few sentences about yourself.</p>
+              <?php if (isset($errors['body'])): ?>
+                <p class="mt-3 text-sm/6 text-red-600"><?= $errors['body'] ?></p>
+              <?php endif ?>
+              <?php if (empty($errors['body'])): ?>
+                <p class="mt-3 text-sm/6 text-gray-600">Write a few sentences about yourself.</p>
+              <?php endif ?>
         </div>        
 
         <div class="mt-6 flex items-center justify-end gap-x-6">
@@ -20,7 +25,6 @@
           <button type="submit" class="rounded-md bg-indigo-600 px-3 py-2 text-sm font-semibold text-white shadow-xs hover:bg-indigo-500 focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-600">Save</button>
         </div>
     </form>
-
   </div>
 </main>
 
