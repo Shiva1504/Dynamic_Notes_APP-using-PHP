afb6397ce9e867d27fec4b6f82cb62e6ad882d54
SHIVA NAIDU <117988290+Shiva1504@users.noreply.github.com>
2025-07-11 22:13:13 +0530

    docs: Update routes.php with comments

--- routes.php
+++ routes.php
@@ -1,24 +1,35 @@
 <?php
 
+//Home
 $router->get('/','index.php');
+// About
 $router->get('/about','about.php');
+// Contact
 $router->get('/contact','contact.php');
 
+//Show all Notes
 $router->get('/notes','notes/index.php')->only('auth');
 
+// View Note
 $router->get('/note','notes/show.php');
+// Delete Note
 $router->delete('/note','notes/destroy.php');
 
+// Create New Note
 $router->get('/create','notes/create.php');
 $router->post('/create','notes/store.php');
 
+// Edit Note
 $router->get('/edit','notes/edit.php');
 $router->patch('/edit','notes/update.php');
 
+// Register Account
 $router->get('/register','registration/create.php')->only('guest');
 $router->post('/register','registration/store.php')->only('guest');
 
+// Login Account
 $router->get('/login','sessions/create.php')->only('guest');
 $router->post('/login','sessions/store.php')->only('guest');
 
+// logout
 $router->delete('/logout','sessions/destroy.php')->only('auth');
